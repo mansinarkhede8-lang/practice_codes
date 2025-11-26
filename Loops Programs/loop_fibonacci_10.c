6cb19453e7a842ed242237ccfe1146c748110169
mansinarkhede8-lang <mansinarkhede8@gmail.com>
2025-11-26 12:59:54 +0530

    Add Fibonacci series program for first 10 numbers
    Implement Fibonacci series to print first 10 numbers.

---
+++ Loops Programs/loop_fibonacci_10.c
@@ -0,0 +1,13 @@
+Programme 8 :Fibonacci series (first 10 numbers)
+#include <stdio.h>
+int main() {
+    int a = 0, b = 1, c;
+    printf("%d %d ", a, b);
+    for(int i = 3; i <= 10; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+    return 0;
+}
