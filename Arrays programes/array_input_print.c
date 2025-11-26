e8894934734bee6e24b7a4f0bcaf7cbb3f9686f5
mansinarkhede8-lang <mansinarkhede8@gmail.com>
2025-11-26 13:57:09 +0530

    Add program to input and print 5 numbers

---
+++ Arrays programes/array_input_print.c
@@ -0,0 +1,14 @@
+Programee 2:Input 5 numbers from user and print
+#include <stdio.h>
+int main() {
+    int arr[5];
+    printf("Enter 5 numbers:\n");
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("You entered: ");
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
