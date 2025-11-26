84f77c274a189a1971632aa15c429959c14f876c
mansinarkhede8-lang <mansinarkhede8@gmail.com>
2025-11-26 11:31:39 +0530

    Add Fibonacci series program in C
    Implement Fibonacci series calculation.

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+Program 7 — Fibonacci Series
+#include <stdio.h>
+int main() {
+    int n, a=0, b=1, c;
+    printf("Enter number of terms: ");
+    scanf("%d",&n);
+
+    printf("Fibonacci Series: %d %d ",a,b);
+
+    for(int i=3; i<=n; i++) {
+        c = a + b;
+        printf("%d ",c);
+        a = b;
+        b = c;
+    }
+    return 0;
+}
