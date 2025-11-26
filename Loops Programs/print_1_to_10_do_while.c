48207d170a61ea1d2409f1d2c1d301908dabb2e6
mansinarkhede8-lang <mansinarkhede8@gmail.com>
2025-11-26 13:11:54 +0530

    Add do-while loop to print numbers from 1 to 10
    This program prints numbers from 1 to 10 using a do-while loop.

---
+++ Loops Programs/print_1_to_10_do_while.c
@@ -0,0 +1,10 @@
+Programee 15: Print numbers from 1 to 10 using do-while loop
+  #include <stdio.h>
+int main() {
+    int i = 1;
+    do {
+        printf("%d ", i);
+        i++;
+    } while(i <= 10);
+    return 0;
+}
