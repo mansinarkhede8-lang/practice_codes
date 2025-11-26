454712d9f0cc17884186ae7a3797620819938d6b
mansinarkhede8-lang <mansinarkhede8@gmail.com>
2025-11-26 14:16:47 +0530

    Create array_duplicates.c

---
+++ Arrays programes/array_duplicates.c
@@ -0,0 +1,16 @@
+Programme 15:Find duplicate elements
+#include <stdio.h>
+int main() {
+    int arr[6] = {1,2,3,2,4,1};
+    printf("Duplicates: ");
+    for(int i=0;i<6;i++) {
+        for(int j=i+1;j<6;j++) {
+            if(arr[i]==arr[j]) {
+                printf("%d ", arr[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
