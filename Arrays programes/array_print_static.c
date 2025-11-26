891b3a5ec7813944b10bbd49a436c2661d891ed3
mansinarkhede8-lang <mansinarkhede8@gmail.com>
2025-11-26 13:56:09 +0530

    Add program to store and print 5 static numbers

---
+++ Arrays programes/array_print_static.c
@@ -0,0 +1,9 @@
+Programee 1:Store and print 5 numbers
+  #include <stdio.h>
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
